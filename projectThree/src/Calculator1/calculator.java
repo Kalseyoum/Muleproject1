0670ce93433a73c0d2e624d00ff2762ae6d59f90
kalta <kalkidant.seyoum@gmail.com>
2020-11-03 18:35:19 -0500

    commit calculator

---
+++ projectThree/src/Calculator1/calculator.java
@@ -0,0 +1,19 @@
+package Calculator1;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		int first = 11;
+        int second = 25;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
